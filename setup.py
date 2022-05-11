218c15676dc7c421200060c2ebd4fb9b9f33bad1
alro <alro@nve.no>
2022-05-11 16:21:12 +0200

    Test

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""Setup file for model tools"""
+
+from setuptools import setup
+
+setup(name='modeltools',
+      version='1.1.0',
+      description='XXXXXX',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python',
+      ],
+      keywords='',
+      author='NVE',
+      author_email='alro@nve.no',
+      license='GPLv3',
+      zip_safe=False,
+      py_modules = ['thema'],
+)
+
